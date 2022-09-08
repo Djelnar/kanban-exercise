c35087d0d98bf70758985f9f11a433e112bf69a4
Elnar Ibrahimov <djelnar@gmail.com>
2022-09-09 00:36:25 +0300

    Dragging ui done

--- app/src/features/board/index.tsx
+++ app/src/features/board/index.tsx
@@ -2,6 +2,13 @@ import { tasks } from "api/mockData";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
+import {
+  DragDropContext,
+  DropResult,
+  ResponderProvided,
+  Draggable,
+  Droppable,
+} from "react-beautiful-dnd";
 import { columnsStore, tasksStore } from "./store";
 import "./style.css";
 import { TaskCard } from "./task-card";
@@ -14,27 +21,56 @@ export function Board() {
   const handleAddNew = () => {
     navigate("/board/new");
   };
+
+  const onDragEnd = (result: DropResult, provided: ResponderProvided) => {
+    console.log(result);
+    console.log(
+      "🚀 ~ file: index.tsx ~ line 23 ~ onDragEnd ~ provided",
+      provided
+    );
+  };
+
   return (
-    <div className="board">
-      {columns.map((column, idx) => (
-        <div className="column" key={column.id}>
-          <p className="columnHeader">{column.name}</p>
-          <div className="columnInner">
-            {tasksGroups[column.id]?.map((task) => (
-              <TaskCard task={task} key={task.id} />
-            ))}
-            {idx === 0 && (
-              <button
-                key="addNewTask"
-                onClick={handleAddNew}
-                className="addNewTask"
-              >
-                Add new
-              </button>
-            )}
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="board">
+        {columns.map((column, idx) => (
+          <div className="column" key={column.id}>
+            <p className="columnHeader">{column.name}</p>
+            <Droppable droppableId={column.id}>
+              {(provided, snapshot) => (
+                <div
+                  className={`columnInner ${
+                    snapshot.draggingOverWith && !snapshot.draggingFromThisWith
+                      ? "columnInnerDraggingOver"
+                      : ""
+                  }`}
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                >
+                  {tasksGroups[column.id]?.map((task, taskIndex) => (
+                    <TaskCard
+                      task={task}
+                      key={task.id}
+                      index={taskIndex}
+                      done={idx === columns.length - 1}
+                    />
+                  ))}
+                  {provided.placeholder}
+                  {idx === 0 && (
+                    <button
+                      key="addNewTask"
+                      onClick={handleAddNew}
+                      className="addNewTask"
+                    >
+                      Add new
+                    </button>
+                  )}
+                </div>
+              )}
+            </Droppable>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </DragDropContext>
   );
 }
--- app/src/features/board/task-card.tsx
+++ app/src/features/board/task-card.tsx
@@ -3,27 +3,39 @@ import { usersStore } from "features/user/store";
 import React from "react";
 import { Link } from "react-router-dom";
 import { useRecoilValue } from "recoil";
+import { Draggable } from "react-beautiful-dnd";
 
 type Props = {
   task: Task;
+  done?: boolean;
+  index: number;
 };
 
-export function TaskCard({ task }: Props) {
+export function TaskCard({ task, done, index }: Props) {
   const usersByKey = useRecoilValue(usersStore);
 
   return (
-    <div className={`card card-${task.importance}`}>
-      <Link to={`/board/${task.id}`} className="cardTitle">
-        {task.name}
-      </Link>
-      <div className="cardUsers">
-        <p className="cardUserTitle">Assigned to:</p>
-        {task.assigned?.map((user) => (
-          <p key={user} className="cardUser">
-            — {usersByKey[user].name}
-          </p>
-        ))}
-      </div>
-    </div>
+    <Draggable draggableId={task.id} index={index}>
+      {(provided, snapshot) => (
+        <div
+          className={`card ${done ? "card-done" : `card-${task.importance}`}`}
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          <Link to={`/board/${task.id}`} className="cardTitle">
+            {task.name}
+          </Link>
+          <div className="cardUsers">
+            <p className="cardUserTitle">Assigned to:</p>
+            {task.assigned?.map((user) => (
+              <p key={user} className="cardUser">
+                — {usersByKey[user].name}
+              </p>
+            ))}
+          </div>
+        </div>
+      )}
+    </Draggable>
   );
 }
